e979b5276f380731753a54ea01c7960fc3890a85
Bhumipat <bhumipat2004@gmail.com>
2023-12-08 20:34:55 +0700

    complted

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,19 +1,29 @@
 #include<iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
+char before(char x) {
+	if (x >= 65 && x <= 90) {
+		if (x == 65) {
+			return 'Z';
+		}
+		else {
+			return x - 1;
+		}
+	}
+	else{
+		return '0';
+	}
 }
 
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
+int main() {
+	cout << before('A') << "\n"; //Z
+	cout << before('B') << "\n"; //
 	cout << before('P') << "\n";
 	cout << before('T') << "\n";
 	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
+	cout << before('a') << "\n"; // 0
 	cout << before('0') << "\n";
 	cout << before('c') << "\n";
+
 	return 0;
 }
